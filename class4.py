95612c047118efe14e3154c6c2c99559a74da656
Justsaymeoww <nattamon.sopak@mail.kmutt.ac.th>
2025-07-24 12:00:33 +0700

    class4

---
+++ class4.py
@@ -0,0 +1,122 @@
+# def add(): ##define function as add ( don't forget to add () )
+#     print(5+5)
+
+# add() ##show output
+
+# def grade(score):      ##score as parameter
+#     if score >= 80:
+#         print("Grade A")
+#     elif score >= 70:
+#         print("Grade B")
+#     elif score >= 60:
+#         print("Grade C")
+#     else:
+#         print("Grade F")
+# grade(60)  ## 60 as argument 
+
+
+# def my_sum (a,b):    ##parameter can be more than 1
+#     print(a+b) ##DO NOT USE 'print' 
+                 ## ONLY USE 'return'
+
+# my_sum(5,5) ## argument can be more than 1 too
+
+
+# def my_sum (a,b):
+#     return a+b
+
+# result = my_sum (5,5)  ## Keep in result
+# print (result) ## print result
+
+
+# def sum(quan,per):
+#     result = quan*per
+#     vat = result * 0.07
+#     total = result + vat
+#     return total
+
+
+# def menu (): ## use interface
+#     while True:
+#         choice = int(input("Enter >>> 1.)Calculator 2.)Exit : "))
+#         if choice == 2:
+#             print("Exited")
+#             break
+
+#         if choice == 1:
+#             quan = int(input("Product Amount : "))
+#             per = int(input("Price per 1 : "))
+#             print(sum(quan,per))            
+# menu()            
+
+
+
+
+# my_list = [1, 2,"Naming"] ## index 0,1,2
+
+# print("before" , my_list[0]) # 0 = index ( output = 1)
+
+# my_list[0] = 3 ### 0 as index
+
+# print("after" , my_list[0])
+# print ("before" , my_list)
+
+
+# # my_list.append("GGG")  ## .append = add more info
+# # my_list.pop()  ## delete last index
+
+# my_list.pop(1) ## delete index1 = 2 in list
+
+# print("After" , my_list)
+
+
+
+# my_list = [1, 2,"Naming"]
+
+# for i in my_list:
+#     print(i)
+
+
+
+# my_dict = {"Name":"Naming", "Age":19}
+# # print(my_dict["Name"])
+# print("before" , my_dict)
+# my_dict["Name"] = "IT"
+# print("after", my_dict)
+
+
+
+# students = [
+#     {"name": "Naming ", "age": 19},
+#     {"name": "Hong" , "age" : 19},
+#     {"name": "Party", "age" : 19}
+# ]
+
+# for i in students:
+#     print(i["name"])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
